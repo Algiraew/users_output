b3b7929acf6b6dc6d75ae687ca2fbfb8e539f6a0
Magomed <algiraev@MacBook-Air-Magomed.local>
2023-01-30 20:08:10 +0300

    install react-router-dom, react-redux, @redux/toolkit, redux-persist, prettier, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-console": "error",
+  },
+};
